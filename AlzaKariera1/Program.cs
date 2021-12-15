92f2a2d1a2a6b2d575b9c9686c3fded8aac9ecb5
rhox <jakub.holomek@centrum.cz>
2021-12-15 13:19:09 +0100

    Add files via upload

---
+++ AlzaKariera1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlzaKariera1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AlzaKariera1Test/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AlzaKarieraTest
+{
+    [TestFixture]
+    public class DemoTest
+    {
+        /*
+         * Na stránkách www.alza.cz/kariera chceme najít všechny inzeráty pro QA oddělení, dle kterých
+         zkontrolujme, že inzerát je správně vyplněn (vyplněný popis pozice, fotografie lidí, které potkáte na pohovoru a jejich
+         krátký popisek). Zjistěte, zda lidi, které v průběhu výběrového řízení potkáte, jsou shodní (fotka / popisek) pro všechny
+         inzeráty na QA oddělení; zkontrolujte, že na stránkách kariéra Alza jsou všechny odkazy funkční.
+         */
+
+        [Test]
+        public static void TestRun()
+        {
+
+        }
+    }
+}
