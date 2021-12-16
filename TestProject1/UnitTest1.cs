887e867fbd80695c9b00f6398b54342e5a9bd138
Holomek Jakub <Jakub.Holomek@embedit.cz>
2021-12-16 15:24:16 +0100

    Setting up project 1

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Configuration;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Runtime.CompilerServices;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver("c:\\Users\\jakub.holomek\\Automation\\Alza\\AlzaKariera");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://www.alza.cz/kariera"; 
+            //var value = ConfigurationManager.AppSettings["SutUrl"];
+        }
+
+
+        public void TakeScreenshot()
+        {
+            Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+            ss.SaveAsFile(ConfigurationManager.AppSettings["ScreenshotUrl"] + DateTime.Now.ToString("yyyy-MM-dd-HH_mm_ss") + ".jpg", ScreenshotImageFormat.Jpeg);
+        }
+    }
+}
